96305b349cdc1dcd8ba42bf20a705177be5efc04
Jirtuoso <86451763+Jirtuoso@users.noreply.github.com>
2024-10-31 16:52:35 -0700

    Update site-config.ts

--- src/site-config.ts
+++ src/site-config.ts
@@ -1,35 +1,19 @@
 export const siteConfig = {
-  author: 'Kevin Wong',
-  title: 'Vitesse theme for Astro',
-  subtitle: 'Vitesse theme for Astro, supports Vue and UnoCSS.',
-  description: 'A Minimal, SEO-friendly portfolio and blog theme for Astro.',
+  author: 'Jay Hou',
+  title: "Jay's golB",
+  subtitle: 'Self-help from an unqualified teacher',
+  description: 'Sharing some personal reflections to the world',
   image: {
     src: '/hero.jpg',
     alt: 'Website Main Image',
   },
-  email: 'kevinwong865@gmail.com',
+  email: 'jayhaswords@gmail.com',
   socialLinks: [
     {
-      text: 'GitHub',
-      href: '',
-      icon: 'i-simple-icons-github',
-      header: 'i-ri-github-line',
-    },
-    {
       text: 'Twitter',
-      href: '',
+      href: 'https://x.com/Koakanino',
       icon: 'i-simple-icons-x',
       header: 'i-ri-twitter-x-line',
-    },
-    {
-      text: 'Linkedin',
-      href: '',
-      icon: 'i-simple-icons-linkedin',
-    },
-    {
-      text: 'Instagram',
-      href: '',
-      icon: 'i-simple-icons-instagram',
     },
     {
       text: 'Youtube',
@@ -51,10 +35,6 @@ export const siteConfig = {
         text: 'Notes',
         href: '/blog/notes',
       },
-      {
-        text: 'Talks',
-        href: '/blog/talks',
-      },
     ],
   },
   page: {
@@ -67,29 +47,17 @@ export const siteConfig = {
         text: 'Notes',
         href: '/blog/notes',
       },
-      {
-        text: 'Talks',
-        href: '/blog/talks',
-      },
     ],
   },
   footer: {
     navLinks: [
       {
-        text: 'Posts Props',
-        href: '/posts-props',
+        text: 'Thanks for Browsing!',
+        href: 'https://ncase.me/door/',
       },
       {
-        text: 'Markdown Style',
-        href: '/md-style',
-      },
-      {
-        text: 'View on Astro',
-        href: 'https://astro.build/themes/details/vitesse-theme-for-astro/',
-      },
-      {
-        text: 'GitHub Repository',
-        href: 'https://github.com/kevinwong865/astro-theme-vitesse',
+        text: 'Goodreads',
+        href: 'https://www.goodreads.com/user/show/127656148-jay',
       },
     ],
   },
